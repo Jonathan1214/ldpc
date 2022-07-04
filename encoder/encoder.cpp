3b6c2f44ec860e6cc090b50334f6c1ebe89b3899
Jiangxuan Li <32216034+Jonathan1214@users.noreply.github.com>
2022-07-05 00:27:31 +0800

    增加编码，待测试

--- encoder/encoder.cpp
+++ encoder/encoder.cpp
@@ -1,3 +1,15 @@
 #include "encoder.h"
 
 
+void 
+Encoder::run_encoder(const LDPC& cc, const info_fram_t& info) {
+    assert(info.size() == cc.get_info_len());
+    assert(coded_o.size() == cc.get_len());
+    for (size_t i = 0; i < cc.get_len(); ++i) {
+        bit_pos_t cur = 0;
+        for (size_t j = 0; j < cc.get_info_len(); ++j) {
+            cur ^= cc.get_gen_xy(i, j) ^ info[j];
+        }
+        coded_o[i] = cur;
+    }
+}
--- encoder/encoder.h
+++ encoder/encoder.h
@@ -1,14 +1,25 @@
+// date: 2022/07/04
+// description: encode for a ldpc code
+//              Read generate matrix from file
+//              Use encoder for code generate
+//
+// bug report contact jonathan1214@foxmail.com
+//
+
 #ifndef _ENCODER_H_
 #define _ENCODER_H_
 #include <iostream>
 #include <vector>
+#include "../LDPCc/ldpc.h"
 
-class encoder {
+class Encoder {
 public:
-    encoder() = default;
+    Encoder() = default;
+    void run_encoder(const LDPC& cc, const info_fram_t& info);
 
+    info_fram_t coded_o; // encoder output
 private:
-    
+
 };
 
 #endif
